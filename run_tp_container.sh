e6490f0516043d3ba379b439ceee489c06b22048
cedric <cedricklinkert@bbv.ch>
2023-10-14 15:45:09 +0200

    Add some security measures

--- run_tp_container.sh
+++ run_tp_container.sh
@@ -4,6 +4,12 @@ docker run \
     --name qtappcontainer \
     --mount type=bind,source=/tmp/.X11-unix,target=/tmp/.X11-unix \
     -e DISPLAY=:0 \
+    --cpus=1 \
+    --memory=100m \
+    --cpu-shares=512 \
+    --read-only \
+    --tmpfs /app/tmp:uid=$(id -u $USER),gid=$(id -g $USER),mode=0700 \
+    --tmpfs /tmp/runtime-dockeruser:uid=$(id -u $USER),gid=$(id -g $USER),mode=0700 \
     -e XDG_RUNTIME_DIR=/app/tmp \
     -u $(id -u $USER):$(id -g $USER) \
     --rm \
